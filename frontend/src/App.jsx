8af47ac13eaba4970b05961802e5503de56de175
LeoMartinDev <7238941+LeoMartinDev@users.noreply.github.com>
2022-10-15 14:59:20 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+
 import { Aria2Provider } from "./Aria2Provider.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
     <Aria2Provider>
-      <div>Salut</div>
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MemoryRouter>
     </Aria2Provider>
   );
 }
