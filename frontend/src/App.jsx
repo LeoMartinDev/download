7ccc1fd694405413fc13b192ae90d344d8a16772
LeoMartinDev <7238941+LeoMartinDev@users.noreply.github.com>
2022-10-15 15:48:46 +0200

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { Aria2Provider } from "./Aria2Provider.jsx";
+import Layout from "./Layout.jsx";
 import Home from "./pages/Home.jsx";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <Aria2Provider>
-      <MemoryRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </MemoryRouter>
+      <RouterProvider router={router} />
+      {/* <MemoryRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </MemoryRouter> */}
     </Aria2Provider>
   );
 }
